97062945f32fae4df840f6f257e45a1b70bbcb90
Pablo Roman <pablo@thenextweb.com>
2017-08-04 11:51:34 +0200

    Fix access to a function on null

--- resources/views/partials/_album-grid-item.blade.php
+++ resources/views/partials/_album-grid-item.blade.php
@@ -12,9 +12,9 @@
         @endif
         <h3 class="album-grid-title"><a href="{{ $album->permalink }}">{{ $album->title }}</a></h3>
         <div class="album-grid-meta">
-
+            @if($album->published_at)
                 {{ $album->published_at->format('Y') }} &middot;
-
+            @endif
             {{ $album->type }}</div>
         <div class="album-grid-meta">{{ $album->label }}</div>
         @includeWhen($album->review_count, 'partials._review-score')
